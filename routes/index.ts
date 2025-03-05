d4b7e9d8a692a796736d192c875a507b339f53cf
Ojo <oluwatobiojo2911@gmail.com>
2025-03-05 16:45:20 +0100

    feat: add category and review routes to main router

--- routes/index.ts
+++ routes/index.ts
@@ -2,11 +2,15 @@ import { Router } from "express";
 import authRouter from "./authRoutes";
 import productRouter from "./productRoutes";
 import userRouter from "./userRoutes";
+import categoryRouter from "./categoryRoutes";
+import reviewRouter from "./reviewRoutes";
 
 const router = Router();
 
 router.use("/api/v1/auth", authRouter);
 router.use("/api/v1/user", userRouter);
 router.use("/api/v1/products", productRouter);
+router.use("/api/v1/categories", categoryRouter);
+router.use("/api/v1/reviews", reviewRouter);
 
 export default router;
